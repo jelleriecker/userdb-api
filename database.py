7bc6e07ea117b5eb0a1dbfafa7ca5bc620995557
jelleriecker <jelle.riecker@nelen-schuurmans.nl>
2024-03-18 23:02:38 +0100

    done

--- database.py
+++ database.py
@@ -1,15 +1,65 @@
 import os
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
+from sqlalchemy import create_engine, Column, Text
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
+
 
 load_dotenv()
 
 DB_URL = os.getenv('DB_URL')
 
 engine = create_engine(DB_URL)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def get_users(db: Session, name: str, limit: int) -> list[dict[str, str]]:
+    # Construct the SQL query
+    results = db.execute(
+        DatabaseUser.__table__.select().where(DatabaseUser.name.like(name)).limit(limit)).fetchall()
+    # Convert the results to a list of dictionaries
+    users = [row._asdict() for row in results]
+
+    return users
+
+
+def create_user(db: Session, name: str, email: str, password_hash: str) -> None:
+    # Create the new user
+    db_user = DatabaseUser(email=email, name=name, password_hash=password_hash)
+
+    try:
+        # Add the new user to the database
+        db.add(db_user)
+        db.commit()
+    except IntegrityError:
+        # If the email already exists, rollback the transaction and return an error
+        db.rollback()
+
+        raise Exception(f"Duplicate e-mail: {email}")
+
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+class DatabaseUser(Base):
+    # noinspection SpellCheckingInspection
+    __tablename__ = "users"
+
+    email = Column(Text, unique=True, primary_key=True, index=True, nullable=False)
+    name = Column(Text, index=True, nullable=False)
+    password_hash = Column(Text, nullable=False)
+
+
+Base.metadata.create_all(bind=engine)
--- main.py
+++ main.py
@@ -1,50 +1,32 @@
 from typing import Annotated
 
-import sqlalchemy
+import bcrypt
+import uvicorn
 from fastapi import FastAPI, Depends, status
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
-from sqlalchemy import text
-from sqlalchemy.engine import row
-from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
-import bcrypt
-import models
-from database import SessionLocal, engine
+import database
+from database import get_db
 
 app = FastAPI()
-models.Base.metadata.create_all(bind=engine)
 
 
-class UserBase(BaseModel):
-    email: text
-    name: text
-    password_hash: text
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+class RequestUser(BaseModel):
+    email: str
+    name: str
+    password: str
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
 @app.get("/users")
-async def get_users(db: Session = Depends(get_db)):
-    def row2dict(row):
-        return {key: row[key] for key in row.keys()}
+async def get_users(query: str = "%", limit: int = 10, db: Session = Depends(get_db)):
     try:
-        # Construct the SQL query
-        sql_query = text("SELECT * FROM users")
-        # Execute the query
-        results = db.execute(sql_query).fetchall()
-        # Convert the results into a list of dictionaries
-        users = [row2dict(row) for row in results]
+        users = database.get_users(db, query, limit)
+
         # Get the total number of users
         total = len(users)
         # Return the results
@@ -53,39 +35,18 @@ async def get_users(db: Session = Depends(get_db)):
         return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={"error": str(e)})
 
 @app.post("/users")
-async def create_user(user: UserBase, db: Session = Depends(get_db)):
+async def create_user(user: RequestUser, db: Session = Depends(get_db)):
     # Hash the password
     password_hash = bcrypt.hashpw(user.password.encode(), bcrypt.gensalt()).decode()
 
-    # Create the new user
-    db_user = models.Users(email=user.email, name=user.name, password_hash=password_hash)
-
     try:
-        # Add the new user to the database
-        db.add(db_user)
-        db.commit()
-    except IntegrityError:
-        # If the email already exists, rollback the transaction and return an error
-        db.rollback()
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={f"Duplicate e-mail: {user.email}"})
+        database.create_user(db, name=user.name, email=user.email, password_hash=password_hash)
+    except Exception as e:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={str(e)})
 
     # If the user was successfully added, return an empty response
-    return {"message": "User created successfully"}
+    return JSONResponse(status_code=status.HTTP_202_ACCEPTED, content={})
 
 
-    # def row2dict(email, name):
-    #     return {key: email[email, name] for key in email.keys()}
-    # try:
-    #     Construct the SQL query
-        # sql_query = text("SELECT * FROM users")
-        # Execute the query
-        # results = db.execute(sql_query).fetchall()
-        # Convert the results into a list of dictionaries
-        # users = [row2dict(email=) for row in results]
-        # Get the total number of users
-        # total = len(users)
-        # Return the results
-        # return JSONResponse(content={"users": users, "total": total})
-    # except Exception as e:
-    #     return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={"error": str(e)})
-
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
